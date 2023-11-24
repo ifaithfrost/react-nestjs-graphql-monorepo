c4752731b99328c122b9c5be7afda10baef56847
renan olovics <renanolovics@gmail.com>
2023-11-24 19:52:05 -0300

    chore: tailwind build on project

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Card } from 'ui'
+
 export const Home = () => {
   return (
-    <main className="flex h-screen w-screen bg-gray-900">
+    <main className="flex h-screen w-screen ">
       <h1>test</h1>
+      <Card />
     </main>
   )
 }
--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 import tailwindAnimate from 'tailwindcss-animate'
 
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', '../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       backgroundImage: {
