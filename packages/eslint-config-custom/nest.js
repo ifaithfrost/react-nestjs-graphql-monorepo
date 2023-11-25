8577939bd0ca1dc2e7e19f3502df7271d42e1202
renan olovics <renanolovics@gmail.com>
2023-11-24 21:14:12 -0300

    feat: back

---
+++ packages/eslint-config-custom/nest.js
@@ -0,0 +1,38 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    // tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {node: true,jest: true},
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        jsxSingleQuote: false,
+        trailingComma: 'es5',
+        allowParens: 'avoid',
+        printWidth: 100,
+        semi: false,
+        plugins: ['prettier-plugin-tailwindcss'],
+      },
+    ],
+  },
+}
